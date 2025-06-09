7c370a5704eebe4ed8721d5a95d5136515e68375
sibellecarvalho <164205216+sibellecarvalho@users.noreply.github.com>
2025-06-09 10:44:08 -0300

    modelo.py

--- modelo.py
+++ modelo.py
@@ -1,31 +1,77 @@
-class Filme:
-    def __init__(self, nome, ano, duracao) :
-        self.nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    def __str__(self):
+        return f'Nome: {self.nome} Likes: {self.likes}'
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
         self.duracao = duracao
-        self.likes = 0
+    
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.duracao} min - Likes: {self.likes}'
 
-def dar_like(self):
-    self.like += 1
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
 
-class Serie:
-    def __init__(self, nome, ano, temporada) :
-        self.nome = nome.title()
-        self.ano = ano
-        self.temporada = temporada
-        self.likes = 0
+    def __str__(self):
+        return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
-def dar_like(self):
-    self.like += 1
 
-vingadores = Filme ('vingadores - guerra infinita', 2018, 160)
-print (f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-       f'- Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
-vingadores.dar_like()
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
 
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vincenzo = Serie('vincenzo', 2021, 1)
+tmep = Filme('todo mundo em panico', 1999, 100)
+demolidor = Serie('demolidor', 2016, 2)
 
-print (f'Nome: {vincenzo.nome} - Ano: {vincenzo.ano} '
-       f'- Temporada: {vincenzo.temporada} - Likes: {vincenzo.likes}')
-vincenzo.dar_like()
-vincenzo.dar_like()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vincenzo.dar_likes()
+vincenzo.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+
+listinha = [vincenzo, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', listinha)
+
+for programa in minha_playlist.listagem:
+    print(programa)
+
+print(f'Tamanho: {len(minha_playlist.listagem)}')
